33caf5e9ec0809fd457b5a879ede48f0bc91a831
Vincent GEORGES <vincent.georges3@etu.univ-lorraine.fr>
2024-10-22 15:50:05 +0200

    ajout filtre entre deux dates, à changer route et methode de récupération des arguments

--- app/src/core/service/spectacle/SpectacleServiceInterface.php
+++ app/src/core/service/spectacle/SpectacleServiceInterface.php
@@ -5,5 +5,6 @@ namespace nrv\core\service\spectacle;
 interface SpectacleServiceInterface
 {
     public function getSpectacles(): array;
+    public function getSpectaclesByDate($dateDebut, $dateFin): array;
 
 }
