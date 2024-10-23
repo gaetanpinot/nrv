77a500d71b3d6398c31c8dad27c42421bae4ea00
Vincent GEORGES <vincent.georges3@etu.univ-lorraine.fr>
2024-10-23 16:14:39 +0200

    supression commentaire inutile et route de test pour le filtrage de date

--- app/config/routes.php
+++ app/config/routes.php
@@ -19,6 +19,7 @@ return function (\Slim\App $app): \Slim\App {
     $app->get('/soirees/{id}[/]', AfficheDetailSoireeAction::class);
 
     $app->get('/spectacles/{id}/soirees[/]', GetSoireesSpectaclesAction::class);
+    
     $app->get('/spectacles[/]', AfficheListeSpectaclesAction::class);
 
     $app->post('/inscription[/]', [InscriptionAction::class, 'inscription']);
@@ -28,8 +29,6 @@ return function (\Slim\App $app): \Slim\App {
     $app->get('/utilisateur/{id}/billets[/]', GetUserBilletsAction::class);
 
     $app->get('/utilisateur/{id}/paniers[/]', GetPanierByIdAction::class);
-
-    $app->get('/spectacles/{dateDeb}/{dateFin}', \nrv\application\actions\AfficheDateSpectacleAction::class);
 
     $app->post('/panier/ajouter-billet', AjouterBilletDansPanierAction::class);
     
