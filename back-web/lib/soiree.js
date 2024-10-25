dc9dfbf439535344e4a29b6118162aa868574043
gaetanpinot <gaetan.pinot6@etu.univ-lorraine.fr>
2024-10-25 11:35:19 +0200

    formulaire créer soiree

--- back-web/lib/soiree.js
+++ back-web/lib/soiree.js
@@ -3,9 +3,27 @@ import {URL_API, URI_THEMES, URI_LIEUX, URI_SPECTACLES} from './settings.js';
 const TEMPLATE_FORM_SOIREE = Handlebars.compile(
 document.querySelector('#templateFormSoiree').innerHTML);
 
-let getInfoSoireeForm = function(){
-console.log("soiree form");
+let handleResp  = function (resp){
+    if(resp.ok){
+        return resp.json();
+    }else{
+        window.alert("Erreur lors de la requete");
+        console.log(resp.body);
+        return null;
+    }
 }
+let getInfoSoireeForm = async function(){
+    let dataForm = {};
+    dataForm.lieux =  await fetch(URL_API+URI_LIEUX).then(handleResp);
+    dataForm.themes =  await fetch(URL_API + URI_THEMES).then(handleResp);
+    dataForm.spectacles =  await fetch(URL_API + URI_SPECTACLES).then(handleResp);
+    document.querySelector('main').innerHTML = TEMPLATE_FORM_SOIREE(dataForm);
+    document.querySelector('#submitSoiree').addEventListener('submit',submitSoireeForm);
+}
+let submitSoireeForm = function(e){
+
+}
+
 
 export function listenerSoireeForm(){
 document.querySelector('#ajouterSoiree').addEventListener('click', getInfoSoireeForm);
