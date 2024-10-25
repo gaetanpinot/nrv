e543a21950415f77d2d7938bc4b260a55e59c1a1
gaetanpinot <gaetan.pinot6@etu.univ-lorraine.fr>
2024-10-25 15:46:08 +0200

    validation des données soiree créée

--- backoffice/src/application/actions/AjouterSoireeAction.php
+++ backoffice/src/application/actions/AjouterSoireeAction.php
@@ -3,6 +3,9 @@
 namespace nrv\back\application\actions;
 
 use DI\Container;
+use Respect\Validation\Exceptions\NestedValidationException;
+use Respect\Validation\Validator;
+use Slim\Exception\HttpBadRequestException;
 use nrv\back\application\renderer\JsonRenderer;
 use nrv\back\core\service\soiree\SoireeService;
 use Psr\Http\Message\ResponseInterface;
@@ -21,7 +24,21 @@ class AjouterSoireeAction extends AbstractAction
     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
     {
         $soiree = $rq->getParsedBody();
-        $this->soireeService->addSoiree($soiree);
-        return JsonRenderer::render($rs, 200, "Ajouté avec succès");
+        $validatorSoiree= Validator::key('nom', Validator::stringType())
+            ->key('id_theme', Validator::intVal())
+            ->key('date', Validator::dateTime('Y-m-d'))
+            ->key('heure_debut', Validator::dateTime('H:i'))
+            ->key('duree', Validator::dateTime('H:i'))
+            ->key('lieu_id', Validator::Uuid())
+            ->key('tarif_normal', Validator::number())
+            ->key('tarif_reduit', Validator::number())
+            ->key('spectacles', Validator::arrayType());
+        try{
+            $validatorSoiree->assert($soiree);
+            $this->soireeService->addSoiree($soiree);
+        }catch(NestedValidationException $e){
+            throw new HttpBadRequestException($rq, $e->getMessage());
+        }
+        return JsonRenderer::render($rs, 201, "Ajouté avec succès");
     }
 }
