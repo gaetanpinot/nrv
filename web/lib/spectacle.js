8c9be7c0d58c44aa8994fb6141b4aebdcb089808
Vincent GEORGES <vincent.georges3@etu.univ-lorraine.fr>
2024-10-25 17:45:24 +0200

    correction choix lieux

--- web/lib/spectacle.js
+++ web/lib/spectacle.js
@@ -29,6 +29,22 @@ function renderTemplate(template, data) {
     const main = document.querySelector('main');
     main.innerHTML = '';
     main.innerHTML = template(data);
+    initFiltres();
+}
+
+function initFiltres() {
+    const select = document.getElementById('filtre-lieu');
+    select.innerHTML = '';
+    select.innerHTML += `<option value="all">Tous</option>`
+    const uri = `${URL_API}/lieux`;
+    fetch(uri)
+        .then((resp) => resp.json())
+        .then((data) => {
+            data.forEach((lieu) => {
+                select.innerHTML += `<option value="${lieu.nom}">${lieu.nom}</option>`;
+            });
+        })
+        .catch((err) => console.error("Erreur lors de la récupération des lieux :", err));
 }
 
 function filtrer() {
