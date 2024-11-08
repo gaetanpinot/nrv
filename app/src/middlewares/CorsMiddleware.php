ff49f90bffab66c7c5a4521fde07906d38114638
Vincent GEORGES <vincent.georges3@etu.univ-lorraine.fr>
2024-11-08 13:50:19 +0100

    modification middleware Cors

--- app/src/middlewares/CorsMiddleware.php
+++ app/src/middlewares/CorsMiddleware.php
@@ -6,15 +6,25 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Routing\RouteContext;
 
 class CorsMiddleware implements MiddlewareInterface{
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
-    {
-		$response = $handler->handle($request);
-		return $response
-			->withHeader('Access-Control-Allow-Origin', '*')
-			->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-			->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-	}
+	
+    public function process(ServerRequestInterface $rq, RequestHandlerInterface $handler ): ResponseInterface {
+        $routeContext = RouteContext::fromRequest($rq);
+        $routingResults = $routeContext->getRoutingResults();
+        $methods = $routingResults->getAllowedMethods();
+        $resquestHeaders = $rq->getHeaderLine('Access-Control-Request-Headers');
+
+        $origin = $rq->hasHeader('Origin') ? $rq->getHeaderLine('Origin') : '*';
+
+        $response = $handler->handle($rq);
+
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', $origin)
+            ->withHeader('Access-Control-Allow-Headers', $resquestHeaders)
+            ->withHeader('Access-Control-Allow-Methods', implode(', ', $methods))
+            ->withHeader('Access-Control-Allow-Credentials', 'true');
+    }
 
 }
